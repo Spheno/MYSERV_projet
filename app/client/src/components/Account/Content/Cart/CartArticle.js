7143b184a13a760a17b57844ad1c1e9ce87a0959
Mohamed L <lakhal.mohamed7@hotmail.fr>
2020-02-26 15:16:01 +0100

    Compteur quantité page panier

--- app/client/src/components/Account/Content/Cart/CartArticle.js
+++ app/client/src/components/Account/Content/Cart/CartArticle.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../../../../styles/cartArticle.css";
 import avatar from "../../../../images/avatar/einstein.jpg";
 import article from "../../../../images/articles/playstation5.jpg";
 
@@ -9,6 +10,7 @@ export default class CartArticle extends React.Component {
 
     this.state = {
       nbArticles: 1,
+      total: null,
       product: {
         id: 1,
         title: "Playstation 5",
@@ -16,7 +18,7 @@ export default class CartArticle extends React.Component {
           "The new gen console that will amaze and entertain all your friends!",
         img: null,
         authorAvatar: null,
-        authorName: "Me",
+        authorName: "Albert",
         date: new Date(
           Date.UTC(
             new Date().getFullYear(),
@@ -25,9 +27,8 @@ export default class CartArticle extends React.Component {
           )
         ),
         options: {
-          weekday: "long",
           year: "numeric",
-          month: "long",
+          month: "numeric",
           day: "numeric"
         },
         category: "",
@@ -39,6 +40,29 @@ export default class CartArticle extends React.Component {
     };
   }
 
+  decrement(e) {
+    const btn = e.target.parentNode.parentElement.querySelector(
+      'button[data-action="decrement"]'
+    );
+    const target = btn.nextElementSibling;
+    let value = Number(target.value);
+
+    if (value - 1 < 1) return false;
+
+    value--;
+    target.value = value;
+  }
+
+  increment(e) {
+    const btn = e.target.parentNode.parentElement.querySelector(
+      'button[data-action="decrement"]'
+    );
+    const target = btn.nextElementSibling;
+    let value = Number(target.value);
+    value++;
+    target.value = value;
+  }
+
   render() {
     let dateFormated = new Intl.DateTimeFormat(
       "default",
@@ -48,7 +72,7 @@ export default class CartArticle extends React.Component {
     return this.state.nbArticles === 0 ? (
       <p>Wow such empty.</p>
     ) : (
-      <>
+      <div className="block w-full">
         <h1 className="text-3xl font-medium leading-snug tracking-wider text-center text-gray-800">
           Number of articles: {this.state.nbArticles}
         </h1>
@@ -71,8 +95,15 @@ export default class CartArticle extends React.Component {
 
           <div className="flex flex-col justify-between p-4 leading-normal bg-white border-b border-l border-r border-gray-400 rounded-b lg:border-l-0 lg:border-t lg:border-gray-400 lg:rounded-b-none lg:rounded-r">
             <div className="mb-8">
+              <div className="float-right px-2 text-white bg-black border rounded-full">
+                {this.state.product.quantity}
+              </div>
               <div className="mb-2 text-xl font-bold text-gray-900">
-                {this.state.product.title} - <span className="text-gray-700">{this.state.product.price}{this.state.product.devise}</span>
+                {this.state.product.title} -{" "}
+                <span className="text-gray-700">
+                  {this.state.product.price}
+                  {this.state.product.devise}
+                </span>
               </div>
               <p className="text-base text-gray-700">
                 {this.state.product.description}
@@ -111,7 +142,32 @@ export default class CartArticle extends React.Component {
                 <p className="text-gray-600">{dateFormated}</p>
               </div>
               <div className="absolute right-0 flex justify-end text-2xl">
-                <div className="px-4 m-2">
+                <div className="w-full px-2 mt-2">
+                  <div className="flex flex-row float-right w-4/12 bg-transparent rounded-lg">
+                    <button
+                      data-action="decrement"
+                      onClick={this.decrement}
+                      className="w-20 text-gray-600 bg-gray-300 rounded-l outline-none cursor-pointer hover:text-gray-700 hover:bg-gray-400"
+                    >
+                      <span className="m-auto text-2xl font-thin">−</span>
+                    </button>{" "}
+                    <input
+                      type="number"
+                      className="flex items-center w-full font-semibold text-center text-gray-700 bg-gray-300 outline-none w- focus:outline-none text-md hover:text-black focus:text-black md:text-basecursor-default"
+                      name="custom-input-number"
+                      min="1"
+                      defaultValue={this.state.product.quantity}
+                    />{" "}
+                    <button
+                      data-action="increment"
+                      onClick={this.increment}
+                      className="w-20 h-full text-gray-600 bg-gray-300 rounded-r cursor-pointer hover:text-gray-700 hover:bg-gray-400"
+                    >
+                      <span className="m-auto text-2xl font-thin">+</span>
+                    </button>
+                  </div>
+                </div>
+                <div className="px-2 m-2">
                   <a
                     className="text-gray-700 no-underline hover:text-red-600"
                     href="#top"
@@ -120,7 +176,7 @@ export default class CartArticle extends React.Component {
                     <FontAwesomeIcon icon="heart" />
                   </a>
                 </div>
-                <div className="px-4 m-2">
+                <div className="px-2 m-2">
                   <a
                     className="text-gray-700 no-underline hover:text-red-600"
                     href="#top"
@@ -133,7 +189,7 @@ export default class CartArticle extends React.Component {
             </footer>
           </div>
         </div>
-      </>
+      </div>
     );
   }
 }
