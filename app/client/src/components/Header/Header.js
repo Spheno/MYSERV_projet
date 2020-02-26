420368235d28efb776547a4fd2208a4c41bd8875
Mohamed L <lakhal.mohamed7@hotmail.fr>
2020-02-26 16:35:13 +0100

    Search bar on header

--- app/client/src/components/Header/Header.js
+++ app/client/src/components/Header/Header.js
@@ -13,6 +13,22 @@ export default class Header extends React.Component {
       <header className="relative z-10 flex flex-col items-center justify-between py-6 sm:flex-row">
         <h3 className="text-2xl font-bold text-blue-900">ShooFly</h3>
         <nav className="hidden text-lg md:flex">
+          <input
+            type="search"
+            className="p-3 bg-gray-200 border-0 rounded shadow"
+            placeholder="Search item, user..."
+          />
+          <div className="absolute top-0 right-0 mt-3 mr-4 text-purple-500">
+            <svg
+              viewBox="0 0 52.966 52.966"
+            />
+            <path
+              d="M51.704,51.273L36.845,35.82c3.79-3.801,6.138-9.041,6.138-14.82c0-11.58-9.42-21-21-21s-21,9.42-21,21s9.42,21,21,21
+        c5.083,0,9.748-1.817,13.384-4.832l14.895,15.491c0.196,0.205,0.458,0.307,0.721,0.307c0.25,0,0.499-0.093,0.693-0.279
+        C52.074,52.304,52.086,51.671,51.704,51.273z M21.983,40c-10.477,0-19-8.523-19-19s8.523-19,19-19s19,8.523,19,19
+        S32.459,40,21.983,40z"
+            />
+          </div>
           <a
             href="/dashboard"
             className="px-6 py-3 text-gray-800 hover:text-purple-300"
