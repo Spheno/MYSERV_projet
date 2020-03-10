d7794eca83560bf87404888591c516cdb969e08a
Mohamed L <lakhal.mohamed7@hotmail.fr>
2020-03-10 15:17:39 +0100

    Header fixed et petite retouche graphique

--- app/client/src/components/Header/Header.js
+++ app/client/src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import API from "../../utils/userAPI";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Sidebar from "./Sidebar";
-import SearchInput from "./SearchInput";
+//import SearchInput from "./SearchInput";
 import "../../styles/burgerMenu.css";
 
 export default class Header extends React.Component {
@@ -18,49 +18,50 @@ export default class Header extends React.Component {
           <a href="/dashboard">ShooFly</a>
         </h3>
         <nav className="hidden text-lg md:flex">
-          <div className="absolute top-0 right-0 justify-between mt-6">
-            <span className="hidden lg:inline">
-              <SearchInput />
+          <div className="absolute top-0 right-0 mt-6">
+            <span className="fixed top-0 right-0 w-auto mt-12 mr-12 bg-gray-100 border shadow">
+              <a
+                href="/dashboard"
+                className="mx-4 my-3 text-2xl text-gray-700 hover:text-purple-300"
+              >
+                <FontAwesomeIcon icon="home" title="Home" />
+                <span className="sr-only">Home</span>
+              </a>
+              <a
+                href="/sales"
+                className="mx-4 my-3 text-2xl text-gray-700 hover:text-purple-300"
+              >
+                <FontAwesomeIcon
+                  icon="money-bill-wave-alt"
+                  title="Your sales"
+                />
+              </a>
+              <a
+                href="/favs"
+                className="mx-4 my-3 text-2xl text-gray-700 hover:text-purple-300"
+              >
+                <FontAwesomeIcon icon="heart" title="Favorites" />
+              </a>
+              <a
+                href="/cart"
+                className="mx-4 my-3 text-2xl text-gray-700 hover:text-purple-300"
+              >
+                <FontAwesomeIcon icon="shopping-cart" title="Cart" />
+              </a>
+              <a
+                href="/profile"
+                className="mx-4 my-3 text-2xl text-gray-700 hover:text-purple-300"
+              >
+                <FontAwesomeIcon icon="user" title="Profile" />
+              </a>
+              <a
+                href="#top"
+                onClick={this.disconnect}
+                className="mx-4 my-3 text-2xl text-gray-700 hover:text-purple-300"
+              >
+                <FontAwesomeIcon icon="sign-out-alt" title="Logout" />
+              </a>
             </span>
-
-            <a
-              href="/dashboard"
-              className="mx-4 my-3 text-2xl text-gray-700 hover:text-purple-300"
-            >
-              <FontAwesomeIcon icon="home" title="Home" />
-              <span className="sr-only">Home</span>
-            </a>
-            <a
-              href="/sales"
-              className="mx-4 my-3 text-2xl text-gray-700 hover:text-purple-300"
-            >
-              <FontAwesomeIcon icon="money-bill-wave-alt" title="Your sales" />
-            </a>
-            <a
-              href="/favs"
-              className="mx-4 my-3 text-2xl text-gray-700 hover:text-purple-300"
-            >
-              <FontAwesomeIcon icon="heart" title="Favorites" />
-            </a>
-            <a
-              href="/cart"
-              className="mx-4 my-3 text-2xl text-gray-700 hover:text-purple-300"
-            >
-              <FontAwesomeIcon icon="shopping-cart" title="Cart" />
-            </a>
-            <a
-              href="/profile"
-              className="mx-4 my-3 text-2xl text-gray-700 hover:text-purple-300"
-            >
-              <FontAwesomeIcon icon="user" title="Profile" />
-            </a>
-            <a
-              href="#top"
-              onClick={this.disconnect}
-              className="mx-4 my-3 text-2xl text-gray-700 hover:text-purple-300"
-            >
-              <FontAwesomeIcon icon="sign-out-alt" title="Logout" />
-            </a>
           </div>
         </nav>
 
